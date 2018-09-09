18b5800937f2b5d9b0477a6198e2a7e9f2aed14d
David Maw <david@maw.name>
2018-09-08 23:29:36 -0700

    Commented out sample code for helping find memory leaks

--- StreamClient/stdafx.h
+++ StreamClient/stdafx.h
@@ -4,6 +4,17 @@
 #define WINVER _WIN32_WINNT_VISTA  // Allow use of features specific to Windows 6 (Vista) or later
 #endif
 
+// The following commented code is for debugging memory leaks
+//#define _CRTDBG_MAP_ALLOC  
+//#include <stdlib.h>  
+//#include <crtdbg.h>
+//#ifdef _DEBUG
+//#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+//// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+//// allocations to be of _CLIENT_BLOCK type
+//#else
+//#define DBG_NEW new
+//#endif
 // Define a bool to check if this is a DEBUG or RELEASE build
 #if defined(_DEBUG)
 const bool debug = true;
