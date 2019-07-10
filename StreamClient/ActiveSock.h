f40ce9025e4e155e3f15dea52c4734b9854d18aa
Blonder <Blonder@users.noreply.github.com>
2019-07-10 12:40:12 +0200

    added override keyword in ActiveSock.h to be more fail safe.

--- StreamClient/ActiveSock.h
+++ StreamClient/ActiveSock.h
@@ -20,16 +20,16 @@ public:
 	void SetSendTimeoutSeconds(int NewTimeoutSeconds);
 	int GetSendTimeoutSeconds();
 	// Receives up to Len bytes of data and returns the amount received - or SOCKET_ERROR if it times out
-	int RecvPartial(LPVOID lpBuf, const ULONG Len);
+	int RecvPartial(LPVOID lpBuf, const ULONG Len) override;
 	// Sends up to Len bytes of data and returns the amount sent - or SOCKET_ERROR if it times out
-	int SendPartial(LPCVOID lpBuf, const ULONG Len);
+	int SendPartial(LPCVOID lpBuf, const ULONG Len) override;
 	// Receives exactly Len bytes of data and returns the amount sent - or SOCKET_ERROR if it times out
 	int RecvMsg(LPVOID lpBuf, const ULONG Len);
 	// Sends exactly Len bytes of data and returns the amount sent - or SOCKET_ERROR if it times out
 	int SendMsg(LPCVOID lpBuf, const ULONG Len);
 	BOOL ShutDown(int nHow = SD_BOTH);
-	DWORD GetLastError();
-	bool Close(void); // Returns true if the close worked
+	DWORD GetLastError() override;
+	bool Close(void) override; // Returns true if the close worked
 
 private:
 	static WSADATA WsaData;
