8fa0202d4db10fd99f4132901535f820249fd7cd
lady mesh <Blonder@users.noreply.github.com>
2019-07-19 23:34:10 +0200

    dtor ISockStream = default

--- StreamServer/ISocketStream.h
+++ StreamServer/ISocketStream.h
@@ -2,7 +2,7 @@
 class ISocketStream
 {
 protected:
-	~ISocketStream() {}; // Disallow polymorphic destruction
+  ~ISocketStream() = default; // Disallow polymorphic destruction
 public:
 	// Receives up to Len bytes of data and returns the amount received - or SOCKET_ERROR if it times out
 	virtual int RecvPartial(LPVOID lpBuf, const size_t Len) = 0;
